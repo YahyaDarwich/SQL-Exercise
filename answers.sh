c9e4bbf24207247b811843dd61b63f8922d4e3a6
Yahya <ydarwich18@gmail.com>
2021-12-12 13:25:33 +0200

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -1,1 +1,21 @@
-grt
+select Name from students;
+
+select * from students
+where Age > 30;
+
+select Name from students
+where Gender = "female"
+And age = 30; 
+
+select Points from students
+where Name = "Alex";
+
+insert into students values ('7','Yahya','23','M','1000');
+
+update students
+set Points = Points+100
+where Name = "Basma";
+
+update students
+set Points = Points-150
+where Name = "Alex";
